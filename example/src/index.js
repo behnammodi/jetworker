62816aede613497e5188d4338a8bf3a0d4798bff
Behnam Mohammadi <itten@live.com>
2019-09-25 15:53:23 +0330

    improve exmaple

--- example/src/index.js
+++ example/src/index.js
@@ -2,4 +2,26 @@ import Client from "./modules/client";
 
 const client = new Client("./worker.js");
 
-client.emit("ADD", { a: 1, b: 2 }, data => console.log("result", data));
+client.emit("ADD", { a: 1, b: 2 }, data =>
+  console.log("ADD result 1+2 =", data)
+);
+
+client.emit("ADD", { a: 2, b: 3 }, data =>
+  console.log("ADD result 2+3 =", data)
+);
+
+client.emit("ADD", { a: 3, b: 4 }, data =>
+  console.log("ADD result 3+4 =", data)
+);
+
+client.emit("MINUS", { a: 2, b: 1 }, data =>
+  console.log("MINUS result 2-1 =", data)
+);
+
+client.emit("MINUS", { a: 4, b: 2 }, data =>
+  console.log("MINUS result 4-2 =", data)
+);
+
+client.emit("MINUS", { a: 4, b: 1 }, data =>
+  console.log("MINUS result 4-1 =", data)
+);
--- example/src/index.worker.js
+++ example/src/index.worker.js
@@ -5,3 +5,7 @@ const service = new Service();
 service.on("ADD", (req, res) => {
   res(req.a + req.b);
 });
+
+service.on("MINUS", (req, res) => {
+  res(req.a - req.b);
+});
